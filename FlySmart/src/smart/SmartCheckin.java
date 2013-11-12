7c4360be4249d14b8bf43dca9318fda1460b3fcd
Alan Scarpellini <alanx1990@yahoo.it>
2013-11-12 09:24:47 +0100

    Sistemata la SmartChecking (Errore nel ciclo "for" e formula non aggiornata)

--- FlySmart/src/smart/SmartCheckin.java
+++ FlySmart/src/smart/SmartCheckin.java
@@ -29,194 +29,205 @@ import fileLock.FileLock;
 public class SmartCheckin {
 	private FileLock voliLock;
 	private HashMap<Integer, FileLock> passLocks, palletLocks;
-	private boolean[][] occupancyPasseggeri= {{false,false,false,false,false,false},{false,false,false,false,false,false},{false,false,false,false,false,false},{false,false,false,false,false,false}};
+	private boolean[][] occupancyPasseggeri= {{false,false,false,false,false,false},{false,false,false,false,false,false},{false,false,false,false,false,false},{false,false,false,false,false,false},{false,false,false,false,false,false},{false,false,false,false,false,false},{false,false,false,false,false,false},{false,false,false,false,false,false},{false,false,false,false,false,false},{false,false,false,false,false,false},{false,false,false,false,false,false},{false,false,false,false,false,false}};
 	private boolean[][] occupancyPallet= {{false,false},{false,false},{false,false},{false,false},{false,false},{false,false},{false,false},{false,false}};
 
-	/**
-	 * crea l'oggetto che implementa l'algoritmo per il calcolo
-	 * dell'assegnazione dei posti e dei pallet
-	 * @param voliLock
-	 * @param passLocks
-	 * @param palletLocks
-	 */
-	public SmartCheckin(FileLock voliLock,
-			HashMap<Integer, FileLock> passLocks,
-			HashMap<Integer, FileLock> palletLocks) {
-		this.voliLock = voliLock;
-		this.passLocks = passLocks;
-		this.palletLocks = palletLocks;
-		
-	}
+/**
+ * crea l'oggetto che implementa l'algoritmo per il calcolo
+ * dell'assegnazione dei posti e dei pallet
+ * @param voliLock
+ * @param passLocks
+ * @param palletLocks
+ */
+public SmartCheckin(FileLock voliLock,
+		HashMap<Integer, FileLock> passLocks,
+		HashMap<Integer, FileLock> palletLocks) {
+	this.voliLock = voliLock;
+	this.passLocks = passLocks;
+	this.palletLocks = palletLocks;
 
-	public void stampaOccupancyPasseggeri(int x){
-		System.out.println("\n\nPasso "+x);
-		for(int i=0; i<occupancyPasseggeri.length; i=i+1) {
-			System.out.println("");
-			for(int j=0; j<occupancyPasseggeri[0].length; j=j+1) {
-				System.out.print(occupancyPasseggeri[i][j] + " ");
-				if(occupancyPasseggeri[i][j]) System.out.print(" ");
-			}
+}
+
+public void stampaOccupancyPasseggeri(int x){
+	System.out.println("\n\nPasso "+x);
+	for(int i=0; i<occupancyPasseggeri.length; i=i+1) {
+		System.out.println("");
+		for(int j=0; j<occupancyPasseggeri[0].length; j=j+1) {
+			System.out.print(occupancyPasseggeri[i][j] + " ");
+			if(occupancyPasseggeri[i][j]) System.out.print(" ");
 		}
 	}
-	
-	public void stampaOccupancyPallet(int x){
-		System.out.println("\n\nPasso "+x);
-		for(int i=0; i<occupancyPallet.length; i=i+1) {
-			System.out.println("");
-			for(int j=0; j<occupancyPallet[0].length; j=j+1) {
-				System.out.print(occupancyPallet[i][j] + " ");
-				if(occupancyPallet[i][j]) System.out.print(" ");
-			}
+}
+
+public void stampaOccupancyPallet(int x){
+	System.out.println("\n\nPasso "+x);
+	for(int i=0; i<occupancyPallet.length; i=i+1) {
+		System.out.println("");
+		for(int j=0; j<occupancyPallet[0].length; j=j+1) {
+			System.out.print(occupancyPallet[i][j] + " ");
+			if(occupancyPallet[i][j]) System.out.print(" ");
 		}
 	}
-	
-	
-	public void calcolaCheckin(int idVolo) throws FlightNotFoundException, IOException{
-		List<Volo> voli = new LinkedList<Volo>();
-		List<Passeggero> passeggeri = new ArrayList<Passeggero>();
-		List<Pallet> pallets = new ArrayList<Pallet>();
-		XMLToObj<Volo> parserXML = new XMLToObj<Volo>(Volo.class);
-
-		//blocca volo
-		voliLock.acquireWriteLock();
-		voli = parserXML.readObj(Options.voliFileName);
-
-		Collections.sort(voli, VoloComparator.ID_ORDER);
-		int pos = Collections.binarySearch(voli,new Integer(idVolo));
-		if (pos < 0)
-			throw new FlightNotFoundException(idVolo); //volo non trovato
-
-		Volo v = voli.get(pos);
-
-		v.setStato(StatoVolo.CLOSED);
-		//salva volo aggiornato
-		XMLCreate<Volo> XMLVoloWriter = new XMLCreate<Volo>();
-		Document VoliDocument = XMLVoloWriter.createFlySmartDocument(voli);
-		try {
-			XMLVoloWriter.printDocument(VoliDocument, Options.voliFileName);
-		} finally {
-			voliLock.releaseWriteLock();
-		}
-
-		//ottieni elenco passeggeri e pallet
-		//passLocks.get(idVolo).acquireWriteLock();
-		palletLocks.get(idVolo).acquireWriteLock();
-
-		//passeggeri = parserXML.createPasseggeroList( String.format(Options.voloPassFileName, idVolo));
-		XMLToObj<Pallet> parserXMLPallet = new XMLToObj<Pallet>(Pallet.class);
-		pallets = parserXMLPallet.readObj(String.format(Options.voloPalletFileName, idVolo));
+}
 
 
-		//calcola disposizione
+public void calcolaCheckin(int idVolo) throws FlightNotFoundException, IOException{
+	List<Volo> voli = new LinkedList<Volo>();
+	List<Passeggero> passeggeri = new ArrayList<Passeggero>();
+	List<Pallet> pallets = new ArrayList<Pallet>();
+	XMLToObj<Volo> parserXML = new XMLToObj<Volo>(Volo.class);
 
+	//blocca volo
+	voliLock.acquireWriteLock();
+	voli = parserXML.readObj(Options.voliFileName);
 
-		//posizionePallet(pallets, v);
-		//posizionePasseggeri(passeggeri, v);
-		
-		//salva risultato su file
-		//passLocks.get(idVolo).releaseWriteLock();
-		palletLocks.get(idVolo).releaseWriteLock();
+	Collections.sort(voli, VoloComparator.ID_ORDER);
+	int pos = Collections.binarySearch(voli,new Integer(idVolo));
+	if (pos < 0)
+		throw new FlightNotFoundException(idVolo); //volo non trovato
+
+	Volo v = voli.get(pos);
+
+	v.setStato(StatoVolo.CLOSED);
+	//salva volo aggiornato
+	XMLCreate<Volo> XMLVoloWriter = new XMLCreate<Volo>();
+	Document VoliDocument = XMLVoloWriter.createFlySmartDocument(voli);
+	try {
+		XMLVoloWriter.printDocument(VoliDocument, Options.voliFileName);
+	} finally {
+		voliLock.releaseWriteLock();
 	}
 
-	public int[] postoLiberoPasseggeri(int colonnaScelta, int rigaScelta){
+	//ottieni elenco passeggeri e pallet
+	//passLocks.get(idVolo).acquireWriteLock();
+	palletLocks.get(idVolo).acquireWriteLock();
 
-		int maxRighe =occupancyPasseggeri.length;
-		int distanzaMassima = maxRighe-rigaScelta-1;
-		if(rigaScelta>distanzaMassima) distanzaMassima=rigaScelta;
+	//passeggeri = parserXML.createPasseggeroList( String.format(Options.voloPassFileName, idVolo));
+	XMLToObj<Pallet> parserXMLPallet = new XMLToObj<Pallet>(Pallet.class);
+	pallets = parserXMLPallet.readObj(String.format(Options.voloPalletFileName, idVolo));
 
-		for(int i=0;i<=distanzaMassima;i++){
-			int colonnaCalcolata = colonnaScelta;
-			for(int vicino=0;vicino<6;vicino++){
-				try{
-					if(!occupancyPasseggeri[rigaScelta+i][colonnaCalcolata]){
-						occupancyPasseggeri[rigaScelta+i][colonnaCalcolata]=true;
-						int[] posto = {rigaScelta+i, colonnaCalcolata};
-						return posto;
-					}
-				}catch(ArrayIndexOutOfBoundsException e){
+
+	//calcola disposizione
+
+
+	//posizionePallet(pallets, v);
+	//posizionePasseggeri(passeggeri, v);
+
+	//salva risultato su file
+	//passLocks.get(idVolo).releaseWriteLock();
+	palletLocks.get(idVolo).releaseWriteLock();
+}
+
+public int[] postoLiberoPasseggeri(int colonnaScelta, int rigaScelta){
+
+	int maxRighe =occupancyPasseggeri.length;
+	int distanzaMassima = maxRighe-rigaScelta-1;
+	if(rigaScelta>distanzaMassima) distanzaMassima=rigaScelta;
+
+	for(int i=0;i<=distanzaMassima;i++){
+		// System.err.println("i:"+i);
+
+		// System.err.println("++++++++++++++++++++++++++");
+		int colonnaCalcolata = colonnaScelta;
+		for(int vicino=0;vicino<6;vicino++){
+			// System.err.println("vicino:"+vicino);
+			try{
+				if(!occupancyPasseggeri[rigaScelta+i][colonnaCalcolata]){
+					occupancyPasseggeri[rigaScelta+i][colonnaCalcolata]=true;
+					int[] posto = {rigaScelta+i, colonnaCalcolata};
+					// System.err.println("++++++++++trovato");
+					return posto;
 				}
-				colonnaCalcolata=(colonnaScelta+(vicino+1))%3+((int)colonnaScelta/3)*3+((int)(vicino+1)/3)*3*(((int)colonnaScelta/3)*(-2)+1);
+			}catch(ArrayIndexOutOfBoundsException e){
 			}
-
-
-
-			colonnaCalcolata = colonnaScelta;
-			for(int vicino=0;vicino<6;vicino++){
-				try{
-					if(!occupancyPasseggeri[rigaScelta-i][colonnaCalcolata]){
-						occupancyPasseggeri[rigaScelta-i][colonnaCalcolata]=true;
-						int[] posto = {rigaScelta-i, colonnaCalcolata};
-						return posto;
-					}
-				}catch(ArrayIndexOutOfBoundsException e){
-				}
-				colonnaCalcolata=(colonnaScelta+vicino)%3+((int)colonnaScelta/3)*3+((int)vicino/3)*3*(((int)colonnaScelta/3)*(-2)+1); //formulazza
-			}
+			colonnaCalcolata=(colonnaScelta+(vicino+1))%3+((int)colonnaScelta/3)*3+((int)(vicino+1)/3)*3*(((int)colonnaScelta/3)*(-2)+1); //l'ultimo passo sarà il vicino 6 ma il for termina
+			// System.err.println("+++++calcolata:"+colonnaCalcolata+" scelta: "+colonnaScelta);
 		}
 
-		int[] posto = {-1, -1};
-		return posto;
+
+		// System.err.println("------------------------------------------");
+		colonnaCalcolata = colonnaScelta;
+		for(int vicino=0;vicino<6;vicino++){
+			try{
+
+				// System.err.println("matrice nel posto"+(rigaScelta-i)+" "+colonnaCalcolata+": "+occupancyPasseggeri[rigaScelta-i][colonnaCalcolata]);
+				if(!occupancyPasseggeri[rigaScelta-i][colonnaCalcolata]){
+					occupancyPasseggeri[rigaScelta-i][colonnaCalcolata]=true;
+					int[] posto = {rigaScelta-i, colonnaCalcolata};
+					// System.err.println("-------trovato"+(rigaScelta-i)+" "+colonnaCalcolata);
+					return posto;
+				}
+			}catch(ArrayIndexOutOfBoundsException e){
+			}
+			colonnaCalcolata=(colonnaScelta+(vicino+1))%3+((int)colonnaScelta/3)*3+((int)(vicino+1)/3)*3*(((int)colonnaScelta/3)*(-2)+1);
+			// System.err.println("-----calcolata:"+colonnaCalcolata+" scelta: "+colonnaScelta);
+		}
 	}
 
-	public int[] postoLiberoPallet(int colonnaScelta, int rigaScelta){
-		int maxRighe =occupancyPallet.length;
-		int distanzaMassima = maxRighe-rigaScelta-1;
-		if(rigaScelta>distanzaMassima) distanzaMassima=rigaScelta;
-		
-		//provo lo scelto
+	int[] posto = {-1, -1};
+	System.out.println("Posto:"+posto[0]+" "+posto[1]);
+	return posto;
+}
+
+public int[] postoLiberoPallet(int colonnaScelta, int rigaScelta){
+	int maxRighe =occupancyPallet.length;
+	int distanzaMassima = maxRighe-rigaScelta-1;
+	if(rigaScelta>distanzaMassima) distanzaMassima=rigaScelta;
+
+	//provo lo scelto
+	try{
+		if(!occupancyPallet[rigaScelta][colonnaScelta]){
+			occupancyPallet[rigaScelta][colonnaScelta]=true;
+			int[] posto = {rigaScelta, colonnaScelta};
+			return posto;
+		}
+	}catch(ArrayIndexOutOfBoundsException e){
+	}
+
+
+	for(int i=0;i<=distanzaMassima;i++){
+
 		try{
-			if(!occupancyPallet[rigaScelta][colonnaScelta]){
-				occupancyPallet[rigaScelta][colonnaScelta]=true;
-				int[] posto = {rigaScelta, colonnaScelta};
+			//Salgo di i e cambio colonna
+			if(!occupancyPallet[rigaScelta-i][1-colonnaScelta]){
+				occupancyPallet[rigaScelta-i][1-colonnaScelta]=true;
+				int[] posto = {rigaScelta-i, 1-colonnaScelta};
 				return posto;
 			}
 		}catch(ArrayIndexOutOfBoundsException e){
 		}
-		
-		
-		for(int i=0;i<=distanzaMassima;i++){
 
-			try{
-				//Salgo di i e cambio colonna
-				if(!occupancyPallet[rigaScelta-i][1-colonnaScelta]){
-					occupancyPallet[rigaScelta-i][1-colonnaScelta]=true;
-					int[] posto = {rigaScelta-i, 1-colonnaScelta};
-					return posto;
-				}
-			}catch(ArrayIndexOutOfBoundsException e){
+		try{
+			//Salgo di i
+			if(!occupancyPallet[rigaScelta+i][colonnaScelta]){
+				occupancyPallet[rigaScelta+i][colonnaScelta]=true;
+				int[] posto = {rigaScelta+i, colonnaScelta};
+				return posto;
 			}
-
-			try{
-				//Salgo di i
-				if(!occupancyPallet[rigaScelta+i][colonnaScelta]){
-					occupancyPallet[rigaScelta+i][colonnaScelta]=true;
-					int[] posto = {rigaScelta+i, colonnaScelta};
-					return posto;
-				}
-			}catch(ArrayIndexOutOfBoundsException e){
-			}
-
-			try{
-				//Scendo di i
-				if(!occupancyPallet[rigaScelta-i][colonnaScelta]){
-					occupancyPallet[rigaScelta-i][colonnaScelta]=true;
-					int[] posto = {rigaScelta-i, colonnaScelta};
-					return posto;
-				}
-			}catch(ArrayIndexOutOfBoundsException e){
-			}
-
-			try{
-				//Scendo di i e cambio colonna
-				if(!occupancyPallet[rigaScelta+i][1-colonnaScelta]){
-					occupancyPallet[rigaScelta+i][1-colonnaScelta]=true;
-					int[] posto = {rigaScelta+i, 1-colonnaScelta};
-					return posto;
-				}
-			}catch(ArrayIndexOutOfBoundsException e){
-			}
+		}catch(ArrayIndexOutOfBoundsException e){
 		}
-		int[] posto = {-1, -1};
-		return posto;
+
+		try{
+			//Scendo di i
+			if(!occupancyPallet[rigaScelta-i][colonnaScelta]){
+				occupancyPallet[rigaScelta-i][colonnaScelta]=true;
+				int[] posto = {rigaScelta-i, colonnaScelta};
+				return posto;
+			}
+		}catch(ArrayIndexOutOfBoundsException e){
+		}
+
+		try{
+			//Scendo di i e cambio colonna
+			if(!occupancyPallet[rigaScelta+i][1-colonnaScelta]){
+				occupancyPallet[rigaScelta+i][1-colonnaScelta]=true;
+				int[] posto = {rigaScelta+i, 1-colonnaScelta};
+				return posto;
+			}
+		}catch(ArrayIndexOutOfBoundsException e){
+		}
 	}
+	int[] posto = {-1, -1};
+	return posto;
+}
 }
--- FlySmart/src/smart/Test.java
+++ FlySmart/src/smart/Test.java
@@ -9,25 +9,58 @@ public class Test {
 	public static void main(String[] args) {
 		
 		
-		/*Prova passeggeri
+		/*Prova passeggeri*/
 		SmartCheckin provaPasseggeri = new SmartCheckin(null, null, null);
-		int[] resultPasseggeri = provaPasseggeri.postoLiberoPasseggeri(1,0);
-		provaPasseggeri.stampaOccupancyPasseggeri(1);
-		resultPasseggeri = provaPasseggeri.postoLiberoPasseggeri(0,0);
-		provaPasseggeri.stampaOccupancyPasseggeri(2);
-		resultPasseggeri = provaPasseggeri.postoLiberoPasseggeri(0,0);
-		provaPasseggeri.stampaOccupancyPasseggeri(3);*/
 		
+		int[] resultPasseggeri = provaPasseggeri.postoLiberoPasseggeri(2,3);
+		resultPasseggeri = provaPasseggeri.postoLiberoPasseggeri(2,3);
+		resultPasseggeri = provaPasseggeri.postoLiberoPasseggeri(2,3);
+		resultPasseggeri = provaPasseggeri.postoLiberoPasseggeri(2,3);
+		resultPasseggeri = provaPasseggeri.postoLiberoPasseggeri(2,3);
+		resultPasseggeri = provaPasseggeri.postoLiberoPasseggeri(2,3);
 		
-		/*Prova pallet*/
-		SmartCheckin provaPallet = new SmartCheckin(null, null, null);
-		int[] resultPallet = provaPallet.postoLiberoPallet(0,4);
-		provaPallet.stampaOccupancyPallet(1);
-		resultPallet = provaPallet.postoLiberoPallet(1,3);
-		provaPallet.stampaOccupancyPallet(2);
-		resultPallet = provaPallet.postoLiberoPallet(1,3);
-		provaPallet.stampaOccupancyPallet(3);
+		resultPasseggeri = provaPasseggeri.postoLiberoPasseggeri(2,5);
+		resultPasseggeri = provaPasseggeri.postoLiberoPasseggeri(2,5);
+		resultPasseggeri = provaPasseggeri.postoLiberoPasseggeri(2,5);
+		resultPasseggeri = provaPasseggeri.postoLiberoPasseggeri(2,5);
+		resultPasseggeri = provaPasseggeri.postoLiberoPasseggeri(2,5);
+		resultPasseggeri = provaPasseggeri.postoLiberoPasseggeri(2,5);
+		
+		resultPasseggeri = provaPasseggeri.postoLiberoPasseggeri(2,5);
+		resultPasseggeri = provaPasseggeri.postoLiberoPasseggeri(2,5);
+		resultPasseggeri = provaPasseggeri.postoLiberoPasseggeri(2,5);
+		resultPasseggeri = provaPasseggeri.postoLiberoPasseggeri(2,5);
+		resultPasseggeri = provaPasseggeri.postoLiberoPasseggeri(2,5);
+		
+		resultPasseggeri = provaPasseggeri.postoLiberoPasseggeri(2,5);
+		resultPasseggeri = provaPasseggeri.postoLiberoPasseggeri(2,5);
+		resultPasseggeri = provaPasseggeri.postoLiberoPasseggeri(2,5);
+		resultPasseggeri = provaPasseggeri.postoLiberoPasseggeri(2,5);
+		resultPasseggeri = provaPasseggeri.postoLiberoPasseggeri(2,5);
+		
+		resultPasseggeri = provaPasseggeri.postoLiberoPasseggeri(2,5);
+		System.out.println("Posto: "+resultPasseggeri[1]+" "+resultPasseggeri[0]);
+		resultPasseggeri = provaPasseggeri.postoLiberoPasseggeri(2,5);
+		System.out.println("Posto: "+resultPasseggeri[1]+" "+resultPasseggeri[0]);
+		resultPasseggeri = provaPasseggeri.postoLiberoPasseggeri(2,5);
+		System.out.println("Posto: "+resultPasseggeri[1]+" "+resultPasseggeri[0]);
 
+		// 		continuiamo da qua!!! 
+		//		resultPasseggeri = provaPasseggeri.postoLiberoPasseggeri(,);
+		//      System.out.println("Posto: "+resultPasseggeri[1]+" "+resultPasseggeri[0]);
+		
+		/*Prova pallet
+		SmartCheckin provaPallet = new SmartCheckin(null, null, null);
+		int[] resultPallet = provaPallet.postoLiberoPallet(0,2);
+		resultPallet = provaPallet.postoLiberoPallet(1,1);
+		resultPallet = provaPallet.postoLiberoPallet(1,0);
+		resultPallet = provaPallet.postoLiberoPallet(0,3);
+		resultPallet = provaPallet.postoLiberoPallet(1,2);
+		resultPallet = provaPallet.postoLiberoPallet(0,1);
+		resultPallet = provaPallet.postoLiberoPallet(0,0);
+		resultPallet = provaPallet.postoLiberoPallet(1,3);
+		System.out.println("Posto: "+resultPallet[1]+" "+resultPallet[0]);
+		 */
 	}
 
 }
